e9e57bbfd8df2700fcdb862597164b6839cddaf1
hadillarous <83988809+hadillarous@users.noreply.github.com>
2021-12-12 12:45:47 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,10 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-           MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            System.out.println(service.reverse("hadil "));
+            System.out.println(service.minchar("hadil "));
+            System.out.println(service.casechanger("HaDil "));
 
         } catch (Exception e) {
             e.printStackTrace();
