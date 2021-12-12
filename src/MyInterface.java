db5cb30169506dc7f660b45774418b9b1b06d016
hadillarous <83988809+hadillarous@users.noreply.github.com>
2021-12-12 12:48:46 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
+    String reverse(String ch) throws RemoteException;
+    char minchar(String ch) throws RemoteException;
+    String casechanger(String ch) throws RemoteException;
 
 }
+
